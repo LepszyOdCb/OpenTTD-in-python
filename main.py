113e72d84c74dfca7e8d724badec9ab9c2a0f646
kal <kapi.zaorski@gmail.com>
2023-09-18 19:29:55 +0200

    l

--- main.py
+++ main.py
@@ -1,149 +1,64 @@
 import pygame
-import sys
+import csv
+import os
 
 # Inicjalizacja Pygame
 pygame.init()
 
-# Stałe
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-GRID_SIZE = 32
-ROWS = SCREEN_HEIGHT // GRID_SIZE
-COLS = SCREEN_WIDTH // GRID_SIZE
+# Ustawienia okna
+screen_width = 800
+screen_height = 800
+cell_size = 32  # Rozmiar komórki w siatce
+rows = 25  # Liczba wierszy
+cols = 25  # Liczba kolumn
 
-# Kolory
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
+# Inicjalizacja okna
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Siatka 25x25")
 
-# Inicjalizacja okna gry
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('OpenTTD-inspired Game')
+# Ścieżka do pliku CSV
+csv_file_path = 'grid_positions.csv'
 
-# Funkcje pomocnicze
-def draw_grid():
-    for x in range(0, SCREEN_WIDTH, GRID_SIZE):
-        pygame.draw.line(screen, BLACK, (x, 0), (x, SCREEN_HEIGHT))
-    for y in range(0, SCREEN_HEIGHT, GRID_SIZE):
-        pygame.draw.line(screen, BLACK, (0, y), (SCREEN_WIDTH, y))
+# Sprawdzenie czy plik CSV istnieje, jeśli nie to utworzenie go
+if not os.path.exists(csv_file_path):
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        fieldnames = ['Row', 'Column', 'Position']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
 
-# Klasy obiektów w grze
-class Track:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+# Słownik przechowujący pozycje dla każdej kratki w siatce
+grid_positions = {(i, j): f'{i + 1},{j + 1}' for i in range(rows) for j in range(cols)}
 
-    def draw(self):
-        pygame.draw.rect(screen, BLACK, (self.x * GRID_SIZE, self.y * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+# Zapisanie danych do pliku CSV
+with open(csv_file_path, 'a', newline='') as csvfile:
+    fieldnames = ['Row', 'Column', 'Position']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-class Platform:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    for (row, col), position in grid_positions.items():
+        writer.writerow({'Row': row + 1, 'Column': col + 1, 'Position': position})
 
-    def draw(self):
-        pygame.draw.rect(screen, RED, (self.x * GRID_SIZE, self.y * GRID_SIZE, GRID_SIZE, GRID_SIZE))
-
-class Train:
-    def __init__(self, track_path):
-        self.track_path = track_path  # Ścieżka pociągu jako lista punktów (x, y)
-        self.current_index = 0  # Indeks aktualnej pozycji na trasie
-        self.position = (self.track_path[self.current_index][0] * GRID_SIZE + GRID_SIZE // 2,
-                         self.track_path[self.current_index][1] * GRID_SIZE + GRID_SIZE // 2)
-        self.speed = 2  # Szybkość pociągu
-
-    def move(self):
-        # Sprawdź, czy pociąg nie osiągnął końca trasy
-        if self.current_index < len(self.track_path) - 1:
-            target_position = (self.track_path[self.current_index + 1][0] * GRID_SIZE + GRID_SIZE // 2,
-                               self.track_path[self.current_index + 1][1] * GRID_SIZE + GRID_SIZE // 2)
-
-            # Sprawdź, czy pociąg jest wystarczająco blisko celu
-            if abs(self.position[0] - target_position[0]) <= self.speed and \
-                    abs(self.position[1] - target_position[1]) <= self.speed:
-                self.current_index += 1
-
-            # Poruszaj pociąg w kierunku celu
-            if self.current_index < len(self.track_path) - 1:
-                dx = target_position[0] - self.position[0]
-                dy = target_position[1] - self.position[1]
-                distance = (dx ** 2 + dy ** 2) ** 0.5
-                self.position = (self.position[0] + dx / distance * self.speed,
-                                 self.position[1] + dy / distance * self.speed)
-
-    def draw(self):
-        pygame.draw.circle(screen, GREEN, (int(self.position[0]), int(self.position[1])), GRID_SIZE // 4)
-
-# Inicjalizacja list przechowujących obiekty
-tracks = []
-platforms = []
-trains = []
-blue_path = []
-
-# Pętla gry
+# Główna pętla gry
 running = True
-selected_mode = None
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            pos = pygame.mouse.get_pos()
-            x = pos[0] // GRID_SIZE
-            y = pos[1] // GRID_SIZE
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # LPM
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            row = mouse_y // cell_size
+            col = mouse_x // cell_size
+            print(f"Kliknięto na pozycji: {row + 1},{col + 1}")
 
-            if selected_mode == 1:
-                track = Track(x, y)
-                tracks.append(track)
-                blue_path.append((x, y))
-            elif selected_mode == 2:
-                platform = Platform(x, y)
-                platforms.append(platform)
-            elif selected_mode == 3:
-                pass  # Pociąg może być dodany tylko po zdefiniowaniu trasy
-            elif selected_mode == 4:
-                pass  # Punkt początkowy/końcowy będzie dodany po zdefiniowaniu trasy
+    # Wyczyszczenie ekranu
+    screen.fill((255, 255, 255))
 
-    # Rysowanie tła
-    screen.fill(WHITE)
-    draw_grid()
+    # Rysowanie siatki
+    for i in range(rows):
+        for j in range(cols):
+            pygame.draw.rect(screen, (0, 0, 0), (j * cell_size, i * cell_size, cell_size, cell_size), 1)
 
-    # Rysowanie tras
-    for track in tracks:
-        track.draw()
-
-    # Rysowanie peronów
-    for platform in platforms:
-        platform.draw()
-
-    # Rysowanie pociągów
-    for train in trains:
-        train.move()
-        train.draw()
-
-    # Rysowanie niebieskiej ścieżki
-    if len(blue_path) > 1:
-        for i in range(len(blue_path) - 1):
-            start_pos = (blue_path[i][0] * GRID_SIZE + GRID_SIZE // 2, blue_path[i][1] * GRID_SIZE + GRID_SIZE // 2)
-            end_pos = (blue_path[i + 1][0] * GRID_SIZE + GRID_SIZE // 2, blue_path[i + 1][1] * GRID_SIZE + GRID_SIZE // 2)
-            pygame.draw.line(screen, BLUE, start_pos, end_pos, 5)
-
-    # Aktualizacja okna
+    # Aktualizacja ekranu
     pygame.display.flip()
 
-    # Obsługa wyboru trybu
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_1]:
-        selected_mode = 1
-    elif keys[pygame.K_2]:
-        selected_mode = 2
-    elif keys[pygame.K_3]:
-        selected_mode = 3
-    elif keys[pygame.K_4]:
-        selected_mode = 4
-
+# Zakończenie pracy z Pygame
 pygame.quit()
-sys.exit()
